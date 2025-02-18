4cc8082aa62657465518152969f3d4ec1dbeac58
Gitanjali Ghugare <gghugare09@gmail.com>
2025-02-18 05:53:10 +0000

    basic tests created and passed.

--- src/salmon_project/social_distribution/tests.py
+++ src/salmon_project/social_distribution/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Author
+import uuid
 
 # Create your tests here.
+class AuthorTests(TestCase):
+
+    def setUp(self):
+        self.author = Author.objects.create(
+            username = "testuser",
+            display_name = "Test User",
+            github = "https://github.com/testuser",
+            profile_image = "https://example.com/profile_image.jpg",
+        )
+    
+    def test_get_author(self):
+        response = self.client.get(reverse('get_author', args=[self.author.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['username'], "testuser")
+
+    def test_update_author(self):
+        response = self.client.post(reverse('update_author', args=[self.author.id]) , {
+            "display_name":"Updated User",
+            "github": "https://github.com/updateduser"
+        }, content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+        self.author.refresh_from_db()
+        self.assertEqual(self.author.display_name, "Updated User")
