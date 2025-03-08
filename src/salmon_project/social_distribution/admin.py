2309941aa88374040e4334a5e5bd023bbe8cb991
Jesugbenga Omoniwa <jesuomo69@gmail.com>
2025-03-08 00:24:34 -0700

    added initial changes for the admin

--- src/salmon_project/social_distribution/admin.py
+++ src/salmon_project/social_distribution/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Author)
+admin.site.register(Post)
+admin.site.register(PostLike)
+admin.site.register(Comment)
+admin.site.register(CommentLike)
+admin.site.register(FollowRequest)
+admin.site.register(FeedBlock)
