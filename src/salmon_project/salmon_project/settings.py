25e375e00e560f12bf0683225a8e9db76b7f7b33
Gitanjali Ghugare <gghugare09@gmail.com>
2025-03-24 13:09:42 -0600

    printing allowed hosts

--- src/salmon_project/salmon_project/settings.py
+++ src/salmon_project/salmon_project/settings.py
@@ -141,3 +141,4 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+print("ALLOWED_HOSTS:", ALLOWED_HOSTS)
