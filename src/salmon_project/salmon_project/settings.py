182102ac2cd3fdd509ff1038a31006173c7e6e9f
Jesugbenga <jesuomo69@gmail.com>
2025-03-25 20:26:23 +0000

    hosting changes

--- src/salmon_project/salmon_project/settings.py
+++ src/salmon_project/salmon_project/settings.py
@@ -32,6 +32,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key-1234")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "0").lower() in ["1", "true"]
 
+ALLOWED_HOSTS = ["*"]
+
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
 
 BASE_URL ="http://127.0.0.1:8000"
