f88e4e70fcb52e48799bd5268c8bc55d31a9c931
Jesugbenga Omoniwa <jesuomo69@gmail.com>
2025-03-29 18:10:05 -0600

    adjusted models adn added tests for malicios user

--- src/salmon_project/social_distribution/models.py
+++ src/salmon_project/social_distribution/models.py
@@ -58,10 +58,6 @@ class Post(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     content = models.TextField(default="")
     text = models.TextField(null=True, blank=True)
-    image = models.ImageField(upload_to="images/", null=True, blank=True,
-                              validators=[FileExtensionValidator(allowed_extensions=["png", "jpg", "jpeg"])])
-    video = models.FileField(upload_to="videos/", null=True, blank=True,
-                             validators=[FileExtensionValidator(allowed_extensions=["mp4", "mov", "avi"])])
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
 
